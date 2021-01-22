3dcbfcee6dd2e303a8c1ac95d82162a0474325fe
JeanCherubini <jeancherubini@gmail.com>
2021-01-22 08:27:37 -0300

    fix break

--- search_all_dataset.py
+++ search_all_dataset.py
@@ -61,4 +61,3 @@ if __name__ == '__main__' :
     for query_class in os.listdir(params.query_path):
         for query_instance in os.listdir(params.query_path + '/' + query_class):
             finder.search_query(params, query_class, query_instance)
-            break
