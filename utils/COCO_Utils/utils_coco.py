ff43402b4be9306af34b5dda0e04da39725b69c1
JeanCherubini <jeancherubini@gmail.com>
2021-01-26 00:04:15 -0300

    fix to add zero annotation for images without annot

--- utils/COCO_Utils/utils_coco.py
+++ utils/COCO_Utils/utils_coco.py
@@ -378,11 +378,12 @@ class Dataset(object):
         annotations = self.image_info[image_id]['annotations']
         annotations_list = []
         for annot in annotations:
-            x1, y1, height, width = annot['bbox']
-            label = annot['category_id']
-            annotations_list.append([x1, y1, height, width, label])
-        if not annotations_list:
-            annotations_list.append([0,0,0,0,0])
+            try:
+                x1, y1, height, width = annot['bbox']
+                label = annot['category_id']
+                annotations_list.append([x1, y1, height, width, label])
+            except:
+                annotations_list.append([0,0,0,0,0])
         return np.array(annotations_list)
 
     def next_power_of_2(self,x):  
