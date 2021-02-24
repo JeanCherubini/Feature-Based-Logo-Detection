c481bca4afa6cbeb1601dbeb00ad1919d3cf2af4
JeanCherubini <jeancherubini@gmail.com>
2021-02-24 15:00:40 -0300

    fixed particular query search program

--- search_query.py
+++ search_query.py
@@ -78,5 +78,5 @@ if __name__ == '__main__' :
 
     finder = query_finder()
 
-    query = finder.get_query(params, query_class, query_instance)
+    query = finder.get_query(params, params.query_class, params.query_instance)
     finder.search_query(params, params.query_class, params.query_instance, query)
